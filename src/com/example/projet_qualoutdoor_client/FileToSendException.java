db7add08fc55c2dc40dc63145fe73b087b5d212b
lucas <lucascxmarie@live.fr>
2014-07-22 11:58:07 +0200

    DATA BASE COMPLETE V2

---
+++ src/com/example/projet_qualoutdoor_client/FileToSendException.java
@@ -0,0 +1,11 @@
+package com.example.projet_qualoutdoor_client;
+
+public class FileToSendException extends Exception {
+
+	
+	private static final long serialVersionUID = 1L;
+
+	public FileToSendException(String message) {
+        super(message);
+  }
+}
